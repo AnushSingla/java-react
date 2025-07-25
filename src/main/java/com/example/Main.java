b6a8c27580d714b03cd013e56a7b4a09bafaa402
AnushSingla <singlaanush18@gmail.com>
2025-07-26 01:42:21 +0530

    final

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,9 @@
-package main.java.com.example;
+package com.example;
 
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Java Setup Working");
     }
+
     
 }
