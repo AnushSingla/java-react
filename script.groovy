8a6c85724ed9635b6263855520d3e4f45f91a116
AnushSingla <singlaanush18@gmail.com>
2025-08-23 02:15:18 +0530

    bug-fix2

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildJar(){
 }
 
 def buildImage(){
-    sh 'docker build -t anushsingla/java-react:jma-2.0'
+    sh 'docker build -t anushsingla/java-react:jma-2.0 .'
     withCredentials([usernamePassword(credentialsId:'docker-hub-repo',passwordVariable:'PASS',usernameVariable:'USER')]){
         sh 'echo $PASS | docker login -u $USER --password-stdin'
     }
