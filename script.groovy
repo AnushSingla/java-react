21bb607dcb8bfd239d666b34cceed1cd0b0ede59
AnushSingla <singlaanush18@gmail.com>
2025-08-29 16:25:12 +0530

    jenkins commit

--- script.groovy
+++ script.groovy
@@ -5,4 +5,21 @@ def deployApp(){
     
 }
 
+def commitv(String newVersion){
+    echo "Committing version changes to git"
+    withCredentials([usernamePassword(credentialsId:"github-credentials",usernameVariable:"USER"
+    , passwordVariable:"PASS")]){
+    
+      sh """ git config --global user.email "singlaanush18@gmail.com"
+             git config --global user.name "Anush"
+      
+       """
+       sh "git remote set-url origin https://${USER}:${PASS}@github.com/AnushSingla/java-react.git"
+       sh "git add ."
+       sh "git commit -m 'ci: bump version to ${newVersion}'"
+       sh "git push origin HEAD:jenkins-build"
+    }
+    
+}
+
 return this
