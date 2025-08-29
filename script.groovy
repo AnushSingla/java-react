f1c98607c19c32a81ee79aeecfdc5ed9f9d836dc
AnushSingla <singlaanush18@gmail.com>
2025-08-29 17:48:40 +0530

    fix

--- script.groovy
+++ script.groovy
@@ -2,26 +2,22 @@ import java.net.URLEncoder
 
 def deployApp(){
     echo "Deploying App"
-    
 }
 
 def commitv(String newVersion){
     echo "Committing version changes to git"
-    withCredentials([usernamePassword(credentialsId:"ec80911f-ca2d-4c90-aabe-a4aa00586d02",usernameVariable:"USER"
-    , passwordVariable:"TOKEN")]){
+    withCredentials([usernamePassword(credentialsId:"ec80911f-ca2d-4c90-aabe-a4aa00586d02", usernameVariable:"USER", passwordVariable:"TOKEN")]) {
+        def encodedToken = URLEncoder.encode(TOKEN, "UTF-8")
 
-    def encodedToken = URLEncoder.encode(TOKEN, "UTF-8")
-    
-      sh """ git config --global user.email "singlaanush18@gmail.com"
-             git config --global user.name "Anush"
-      
-       """
-       sh "git remote set-url origin https://${USER}:${encodedToken}@github.com/AnushSingla/java-react.git"
-       sh "git add ."
-       sh "git commit -m 'ci: bump version to ${newVersion}'"
-       sh "git push origin HEAD:jenkins-build"
+        sh """
+            git config --global user.email "singlaanush18@gmail.com"
+            git config --global user.name "Anush"
+        """
+        sh "git remote set-url origin https://${USER}:${encodedToken}@github.com/AnushSingla/java-react.git"
+        sh "git add ."
+        sh "git commit -m 'ci: bump version to ${newVersion} [ci skip]' || echo 'No changes to commit'"
+        sh "git push origin HEAD:jenkins-build"
     }
-    
 }
 
 return this
