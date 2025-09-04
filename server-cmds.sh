87f9d299d74c88faee7a51a3010dee3f7676d7c0
AnushSingla <singlaanush18@gmail.com>
2025-09-05 02:12:20 +0530

    done

--- server-cmds.sh
+++ server-cmds.sh
@@ -7,5 +7,8 @@ docker rm -f java-react || true
 # Pull the new image (IMAGE_TAG is passed by Jenkins)
 docker pull anushsingla/java-react:$IMAGE_TAG
 
+# Update docker-compose.yaml to use the correct image tag
+sed -i "s#image: anushsingla/java-react:.*#image: anushsingla/java-react:$IMAGE_TAG#" docker-compose.yaml
+
 # Start container with docker-compose
-IMAGE_TAG=$IMAGE_TAG docker-compose up -d
+docker-compose up -d
